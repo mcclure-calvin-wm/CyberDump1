9f55545ead06b289dc7506895765032eba087f8f
Start Coding Student 1 <session1@START-Coding-MBP-05.local>
2015-12-10 13:56:03 -0700

    Committed

---
+++ freehtml5buildings/FormScript.js
@@ -0,0 +1,12 @@
+/**
+ * Created by session1 on 12/10/15.
+ */
+
+
+(function(){
+    var fN = document.getElementById("firstNameSection");
+    var lN = document.getElementById("lastNameSection");
+    var pS = document.getElementById("passwordSection");
+    var eS = document.getElementById("emailSection");
+    
+})();
