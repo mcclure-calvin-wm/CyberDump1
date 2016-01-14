ed7ab892263a1bf5680e7d9c7da238d30a72237b
Calvin McClure <session1@START-Coding-MBP-16.local>
2016-01-14 14:45:42 -0700

    Create account works

--- freehtml5buildings/createAccount.php
+++ freehtml5buildings/createAccount.php
@@ -10,10 +10,15 @@
     <!-- modernizr enables HTML5 elements and feature detects -->
     <script type="text/javascript" src="js/modernizr-1.5.min.js"></script>
     <?php
+    try {
+        $dbh = new PDO('mysql:host=127.0.0.1;dbname=cyberdump', 'root', 'root');
 
-    include_once('connect.php');
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
 
-    if(@$_POST['createAccount'] == "Submit")
+    if(@$_POST['createAccounts'] == "Submit")
     {    $errorMessage = "";
 
         if(empty($_POST['firstname']))
@@ -24,30 +29,28 @@
         {
             $errorMessage = "<li>You forgot to enter your last name.</li>";
         }
+        if(empty($_POST['email']))
+        {
+        $errorMessage = "<li>You forgot to enter an email</li>";
+        }
         if(empty($_POST['username']))
         {
             $errorMessage = "<li>You forgot to enter your username.</li>";
         }
-        if(empty($_POST['email']))
-        {
-            $errorMessage = "<li>You forgot to enter an email</li>";
-        }
-
         if(empty($_POST['password']))
         {
             $errorMessage = "<li>You forgot to enter a password</li>";
         }
 
+        $stmt = $dbh->prepare("INSERT INTO createAccount (firstname, lastname, email, username, password) VALUES (?, ?, ?, ?, ?)");
 
-        $stmt = $dbh->prepare("INSERT INTO createuser (firstname, lastname, username, email, password) VALUES (?, ?, ?, ?, ?)");
-
-        $result = $stmt->execute(array($_POST['firstname'], $_POST['lastname'], $_POST['username'], $_POST['email'], $_POST['password']));
+        $result = $stmt->execute(array($_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['username'], $_POST['password']));
 
         if(!$result){
             print_r($stmt->errorInfo());
         }
         {
-            echo("<p>There was an error with your form:</p>\n");
+            echo("");
             echo("<ul>" . $errorMessage . "</ul>\n");
         }
 
@@ -81,7 +84,7 @@
     </div>
 
     <div id="Accountsetup">
-        <form name="createAccount" method="post">
+        <form name="createAccount" action="createAccount.php" method="post">
             <h5>Sign Up</h5>
             <p>First name</p>
             <input type="text" name="firstname">
@@ -89,15 +92,15 @@
             <input type="text" name="lastname">
             <p>Email</p>
             <input type="text" name="email">
+            <p>Username</p>
+            <input type="text" name="username">
             <p>Password</p>
             <input type="password" name="password">
-            <p>confirm password</p>
-            <input type="password" name="confirmPassword">
+            <!--<p>confirm password</p>
+            <input type="password" name="confirmPassword">-->
             <br>
             <br>
-            <input type="submit" name="createAccount" value="Create Account"
-                ></input>
-            <p style="color:transparent;">Secret</p>
+            <input type="submit" name="createAccounts" value="Submit">
         </form>
     </div>
 
